8c3594992bb2a37f8fed4caa27ae32e0d6ea2aa5
Yemi Fasuyi <yemi.fasuyi@gasway.co.uk>
2020-04-16 10:18:01 +0100

    OrderControl - fixed for each loop

--- FSM/Business/Orders/OrderControl.cs
+++ FSM/Business/Orders/OrderControl.cs
@@ -9,7 +9,6 @@ using FSM.Entity.Jobs;
 using FSM.Entity.Orders;
 using FSM.Entity.Sys;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -17,70 +16,63 @@ using System.Runtime.CompilerServices;
 
 namespace FSM.Business.Orders
 {
-  public class OrderControl
-  {
-    private Order _order;
-
-    public OrderControl(Order order)
+    public class OrderControl
     {
-      this._order = order;
-    }
+        private Order _order;
 
-    public bool IsWithinJobSpendLimit()
-    {
-      Job byOrderId = App.DB.Job.Job_Get_ByOrderID(this._order.OrderID);
-      bool flag;
-      if (byOrderId == null)
-      {
-        flag = true;
-      }
-      else
-      {
-        Decimal jobSpendLimit = this.GetJobSpendLimit(byOrderId.JobID);
-        flag = Decimal.Compare(jobSpendLimit, Decimal.MinusOne) == 0 || Decimal.Compare(this.GetCurrentJobCost(byOrderId.JobID), jobSpendLimit) <= 0;
-      }
-      return flag;
-    }
-
-    private Decimal GetJobSpendLimit(int jobId)
-    {
-      Decimal num = Decimal.MinusOne;
-      List<CostCentre> source = App.DB.CostCentre.Get(jobId, 0, FSM.Entity.CostCentres.Enums.GetBy.JobId);
-      CostCentre costCentre = source != null ? source.FirstOrDefault<CostCentre>() : (CostCentre) null;
-      if (costCentre != null && Decimal.Compare(costCentre.JobSpendLimit, Decimal.Zero) > 0)
-      {
-        num = costCentre.JobSpendLimit;
-      }
-      else
-      {
-        FSM.Entity.Customers.Customer byOrderId = App.DB.Customer.Customer_Get_ByOrderID(this._order.OrderID);
-        if (byOrderId != null && Decimal.Compare(byOrderId.JobSpendLimit, Decimal.Zero) > 0)
-          num = byOrderId.JobSpendLimit;
-      }
-      return num;
-    }
-
-    private Decimal GetCurrentJobCost(int jobId)
-    {
-      DataView currentCostByJobId = App.DB.EngineerVisitsPartsAndProducts.Get_CurrentCost_ByJobID(jobId);
-      Decimal num = new Decimal();
-      IEnumerator enumerator;
-      try
-      {
-        enumerator = currentCostByJobId.Table.Rows.GetEnumerator();
-        while (enumerator.MoveNext())
+        public OrderControl(Order order)
         {
-          DataRow current = (DataRow) enumerator.Current;
-          if (!Helper.MakeStringValid(RuntimeHelpers.GetObjectValue(current["Part"])).Contains("IBC"))
-            num = new Decimal(Convert.ToDouble(num) + Helper.MakeDoubleValid(RuntimeHelpers.GetObjectValue(current["Cost"])));
+            this._order = order;
         }
-      }
-      finally
-      {
-        if (enumerator is IDisposable)
-          (enumerator as IDisposable).Dispose();
-      }
-      return num;
+
+        public bool IsWithinJobSpendLimit()
+        {
+            Job byOrderId = App.DB.Job.Job_Get_ByOrderID(this._order.OrderID);
+            bool flag;
+            if (byOrderId == null)
+            {
+                flag = true;
+            }
+            else
+            {
+                Decimal jobSpendLimit = this.GetJobSpendLimit(byOrderId.JobID);
+                flag = Decimal.Compare(jobSpendLimit, Decimal.MinusOne) == 0 || Decimal.Compare(this.GetCurrentJobCost(byOrderId.JobID), jobSpendLimit) <= 0;
+            }
+            return flag;
+        }
+
+        private Decimal GetJobSpendLimit(int jobId)
+        {
+            Decimal num = Decimal.MinusOne;
+            List<CostCentre> source = App.DB.CostCentre.Get(jobId, 0, FSM.Entity.CostCentres.Enums.GetBy.JobId);
+            CostCentre costCentre = source != null ? source.FirstOrDefault<CostCentre>() : (CostCentre)null;
+            if (costCentre != null && Decimal.Compare(costCentre.JobSpendLimit, Decimal.Zero) > 0)
+            {
+                num = costCentre.JobSpendLimit;
+            }
+            else
+            {
+                FSM.Entity.Customers.Customer byOrderId = App.DB.Customer.Customer_Get_ByOrderID(this._order.OrderID);
+                if (byOrderId != null && Decimal.Compare(byOrderId.JobSpendLimit, Decimal.Zero) > 0)
+                    num = byOrderId.JobSpendLimit;
+            }
+            return num;
+        }
+
+        private Decimal GetCurrentJobCost(int jobId)
+        {
+            DataView currentCostByJobId = App.DB.EngineerVisitsPartsAndProducts.Get_CurrentCost_ByJobID(jobId);
+            Decimal num = new Decimal();
+
+            foreach (DataRow dr in currentCostByJobId.Table.Rows)
+            {
+                if (!Helper.MakeStringValid(RuntimeHelpers.GetObjectValue(dr["Part"])).Contains("IBC"))
+                {
+                    num = new Decimal(Convert.ToDouble(num) + Helper.MakeDoubleValid(RuntimeHelpers.GetObjectValue(dr["Cost"])));
+                }
+            }
+
+            return num;
+        }
     }
-  }
 }
