462f72eb9a804097319e6765bae6827d4024a3cb
Yemi Fasuyi <yemi.fasuyi@gasway.co.uk>
2020-04-30 09:41:25 +0100

    vb to c#

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -21,15 +21,15 @@ using System.Runtime.InteropServices;
 [assembly: Guid("CB95A195-066B-4766-88AF-814055291CA9")]
 
 // Version information for an assembly consists of the following four values:
-// 
+//
 // Major Version
 // Minor Version
 // Build Number
 // '      Revision
-// 
+//
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.12.1.0")]
+[assembly: AssemblyVersion("1.12.7.1")]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-GB")]
