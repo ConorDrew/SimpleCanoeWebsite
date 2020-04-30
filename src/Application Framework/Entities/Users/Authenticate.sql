85e4b015bc8dfb58328940144b4b0fc676cb3b51
Conor Drew <Conor.Drew@flagship-group.co.uk>
2020-04-30 09:38:45 +0100

    Deleted:
    Application Framework > Entities

--- src/Application Framework/Entities/Users/Authenticate.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-UserID 
-FROM tblUser
-WHERE Deleted = 0 
-AND Enabled = 1 
-AND UserName = ?UserName
-AND Password = ?Password
--- src/Application Framework/Entities/Users/LastLogon.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-MAX(AccessDate) AS 'LastLogon' 
-FROM tblhistory 
-WHERE UserID = ?UserID 
-AND AccessType = 'LOGON'
